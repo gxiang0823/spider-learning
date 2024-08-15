8fe631d450c2d95a78356bcb62d00edf6f91df51
guo.xiang <guo.xiang@siemens.com>
2024-08-15 16:43:38 +0800

    update in siemens

--- PRV_Asignee_Configurator.py
+++ PRV_Asignee_Configurator.py
@@ -1,16 +1,23 @@
+import sys
 import urllib.parse
 import urllib.request
+import pandas as pd
 import requests
 import json
+
+requests.packages.urllib3.disable_warnings()
+
 
 def down_load(data_json):
     formatted_data = json.dumps(data_json, indent=4, ensure_ascii=False)
     with open('cp_validation.json', 'w', encoding='utf-8') as fp:
         fp.write(formatted_data)
 
+
 def Get_Request(release, pbu, validation, cookie):
     get_url = 'https://nexus.net.plm.eds.com:7373/cp-validation?'
-    get_data = {'search': 'release=="' + release + '";changePackage.pbu=="' + pbu + '";validation.status=="' + validation + '";flag=in=(C,F)'}
+    get_data = {
+        'search': 'release=="' + release + '";changePackage.pbu=="' + pbu + '";validation.status=="' + validation + '";flag=in=(C,F)'}
     get_data = urllib.parse.urlencode(get_data)
     G_url = get_url + get_data
     get_headers = {'Cookie': cookie}
@@ -18,15 +25,16 @@ def Get_Request(release, pbu, validation, cookie):
     get_request = urllib.request.Request(url=G_url, headers=get_headers)
     get_response = urllib.request.urlopen(get_request)
     get_content = get_response.read().decode('utf-8')
-    data_json = json.loads(get_content)
-    # down_load(data_json)
+    get_data_json = json.loads(get_content)
+    # down_load(get_data_json)
 
     Call_ID_Set = set()
-    for item in data_json:
+    for item in get_data_json:
         if item["callId"] is not None:
             Call_ID_Set.add(str(item["callId"]))
 
     return Call_ID_Set
+
 
 def Post_Request(Call_ID_Set, cookie):
     post_url = 'https://nxrelmgtapi.net.plm.eds.com:7373/calls/search?'
@@ -43,7 +51,7 @@ def Post_Request(Call_ID_Set, cookie):
         post_query += (callId + ',')
     post_query = post_query[:-1] + ')'
     post_payload = '''{"query":"''' + post_query \
-                  + '''","aggregations":[],"filters":[],"fields":[]}'''
+                   + '''","aggregations":[],"filters":[],"fields":[]}'''
 
     post_response = requests.post(
         post_url,
@@ -54,9 +62,72 @@ def Post_Request(Call_ID_Set, cookie):
         timeout=6000
     )
     post_content = post_response.content
-    data_json = json.loads(post_content)
-    print(data_json)
+    if post_content == '[]':
+        print("*************************************************************")
+        print("* Request PR List is Empty, Please try again.               *")
+        print("*************************************************************")
+        sys.exit(0)
+    post_data_json = json.loads(post_content)
+    down_load(post_data_json)
+    # print(post_data_json)
 
+    return post_data_json
+
+def Get_Leader_Info(leader, Assignee_Info):
+    print(leader)
+    print(Assignee_Info)
+
+def Data_Process(post_data_json):
+    Rows = []
+    content_list = post_data_json['content']
+    for ct in content_list:
+        call_type = ct['currentCallType']
+        application = ct['application']
+        if call_type['value'] == 'ER':
+            Call_Type = 'ER'
+            er = ct['er']
+            assignee = er['assignee']
+            Call_ID = str(er['id'])
+            Assignee_ID = str(assignee['id'])
+            Assignee_Email = str(assignee['email'])
+            Leader_ID = str(assignee['leaderId'])
+            Supervisor_ID = str(assignee['supervisorId'])
+            Rows.append({'Call_ID': Call_ID, 'Call_Type': Call_Type, 'Assignee_ID': Assignee_ID,
+                         'Assignee_Email': Assignee_Email, 'Leader_ID': Leader_ID, 'Supervisor_ID':Supervisor_ID, 'application':application})
+        elif call_type['value'] == 'PR':
+            Call_Type = 'PR'
+            pr = ct['pr']
+            assignee = pr['assignee']
+            Call_ID = str(pr['id'])
+            Assignee_ID = str(assignee['id'])
+            Assignee_Email = str(assignee['email'])
+            Leader_ID = str(assignee['leaderId'])
+            Supervisor_ID = str(assignee['supervisorId'])
+            Rows.append({'Call_ID': Call_ID, 'Call_Type': Call_Type, 'Assignee_ID': Assignee_ID,
+                         'Assignee_Email': Assignee_Email, 'Leader_ID': Leader_ID, 'Supervisor_ID':Supervisor_ID, 'application':application})
+    Final_Result = pd.DataFrame(Rows)
+
+    pr_groups = {leader_id : group for leader_id, group in Final_Result.groupby('Leader_ID')}
+
+    original_stdout = sys.stdout
+    with open('result.txt','w') as file:
+        sys.stdout = file
+        for leader, item in pr_groups.items():
+            print(f"Leader_ID:   {leader}")
+            print(item[['Call_ID','Call_Type','Assignee_ID','Assignee_Email']])
+            PR_Num = item[item['Call_Type'] == 'PR' ].shape[0]
+            ER_Num = item[item['Call_Type'] == 'ER' ].shape[0]
+            Assignee_Info = item['Assignee_ID'].unique()
+            print(f"PR Num: {PR_Num}    ER Num: {ER_Num}    total assignee: {len(Assignee_Info)}")
+            print("\n")
+
+        for leader, item in pr_groups.items():
+            Assignee_Info = item['Assignee_ID'].unique()
+            Leader_Info = Get_Leader_Info(leader, Assignee_Info)
+            # Assignee_output = ", ".join(str(assignee) for assignee in Assignee_Info)
+            # print(f"assignee_id: [{Assignee_output}]")
+
+    sys.stdout =  original_stdout
 
 
 if __name__ == '__main__':
@@ -72,12 +143,13 @@ if __name__ == '__main__':
     # print("****************************************************************")
 
     # test code
-    release = "2406.3000"
+    release = "2406.4000"
     pbu = "cam"
     validation = "Pass"
-    cookie = "JSESSIONID=894ACADFC6C8541EFC8F304F7A0DB013"
+    cookie = "JSESSIONID=8D96F4275A4ECBD4129808134F6A8906"
 
     Call_ID_Set = Get_Request(release, pbu, validation, cookie)
 
-    Post_Request(Call_ID_Set, cookie)
+    post_data_json = Post_Request(Call_ID_Set, cookie)
 
+    Data_Process(post_data_json)
